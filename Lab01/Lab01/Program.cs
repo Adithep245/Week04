84ad3e362aae7c842cb726aa7827f2bea6c2762a
sunhajung <58030245@kmitl.ac.th>
2017-10-24 23:04:54 +0700

    14. การใช้งานข้อมูลชนิด boolean (1)

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-namespace variableProperties
+namespace RandomNumber
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data type : Byte");
-            Console.WriteLine("Size :" + sizeof(Byte));
-            Console.WriteLine("Minimum Value :" + Byte.MinValue);
-            Console.WriteLine("Maximum Value :" + Byte.MaxValue);
+            Random random = new Random();
+            int randomNumber = random.Next(0, 9);
+            Console.WriteLine(randomNumber);
         }
     }
 }
