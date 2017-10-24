aaac93fa6ec375fe0bb13f54075ea18eb5e46cb5
sunhajung <58030245@kmitl.ac.th>
2017-10-24 23:43:23 +0700

    ข้อ 17 หาผลลัพธ์สมการ 10 ข้อ

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -3,13 +3,18 @@ public class intergerTest
 {
     static void Main(string[] args)
     {
-        int a, b, c, d, e, f;
-        a = 1;
-        b = a + 6;
-        c = b - 3;
-        d = c * 2;
-        e = d / 2;
-        f = e % 2;
-        Console.WriteLine("a={0}\n,b={1}\n,c={2}\n,d={3}\n,e={4}\n,f={5}\n",a ,b ,c ,d ,e ,f);
+        int a = 10, b = 20, x = 5, y = 2 ,Ans1 , Ans2, Ans3, Ans4, Ans5, Ans6, Ans7, Ans8,Ans9, Ans10;
+        Ans1 = a + b;
+        Ans2 = x - b;
+        Ans3 = x * b;
+        Ans4 = y / a;
+        Ans5 = b % y;
+        Ans6 = y + 10 % x;
+        Ans7 = a / 3 * 5;
+        Ans8 = 9 / 2 * a;
+        Ans9 = y % 8;
+        Ans10 = 100 * x + y % 2 - a;
+        Console.WriteLine("a={0}\nb={1}\nx={2}\ny={3}\nAns1={4}\nAns2={5}\nAns3={6}\nAns4={7}\nAns5={8}\nAns6={9}\nAns7={10}\nAns8={11}\nAns9={12}\nAns10={13}\n"
+            , a, b, x, y, Ans1, Ans2, Ans3, Ans4, Ans5, Ans6, Ans7, Ans8, Ans9, Ans10);
     }
 }
