c24f42a528a0fb99680e56b0e16c92b7f1c5a5dd
sunhajung <58030245@kmitl.ac.th>
2017-10-24 23:14:22 +0700

    ทำแบบฝึกหัด ข้อ 14

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,9 +5,11 @@ namespace RandomNumber
     {
         static void Main(string[] args)
         {
+            int A = 0;
             Random random = new Random();
             int randomNumber = random.Next(0, 9);
-            Console.WriteLine(randomNumber);
+            bool B = A < randomNumber;
+            Console.WriteLine("Answer A < randomNumber = {0}", B);
         }
     }
 }
